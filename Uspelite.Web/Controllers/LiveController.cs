d4075750e543fd48aa01f7f0521c3c8de67a3820
lexsideris@gmail.com <lexsideris@gmail.com>
2016-02-01 15:24:28 +0200

    Added live controller

---
+++ Uspelite.Web/Controllers/LiveController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uspelite.MVC.Controllers
+{
+    using System.Web.Mvc;
+
+    public class LiveController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
