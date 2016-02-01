e0afffefbc99b630a9f5faccf713446a4ac6703e
lexsideris@gmail.com <lexsideris@gmail.com>
2016-02-01 13:24:10 +0200

    Initial commit

---
+++ Uspelite.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Uspelite.Web.MvcApplication" Language="C#" %>
---
+++ Uspelite.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Uspelite.Web.Startup))]
+namespace Uspelite.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
