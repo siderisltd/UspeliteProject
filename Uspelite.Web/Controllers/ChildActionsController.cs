0c00b88e4926d3d4a336607d472c9c157db65dcb
lexsideris@gmail.com <lexsideris@gmail.com>
2016-02-12 16:44:30 +0200

    Extracted _Sidebar into Child action with cache. Rewrited with Razor. Fixed namespaces in Account views

--- Uspelite.Web/Controllers/ChildActionsController.cs
+++ Uspelite.Web/Controllers/ChildActionsController.cs
@@ -1,15 +1,11 @@
 ﻿namespace Uspelite.Web.Controllers
 {
-    using System;
-    using System.Collections.Generic;
+
     using System.Linq;
     using System.Web.Mvc;
     using Infrastructure.Mapping.Contracts;
     using Models.ChildActions;
-    using Models.Home;
     using Services.Data.Contracts;
-    using Data.Models;
-    using Models.Categories;
 
     [ChildActionOnly]
     public class ChildActionsController : Controller
@@ -21,13 +17,19 @@
             this.postsService = postsService;
         }
 
-        //[OutputCache(Duration = 5 * 60, VaryByParam = "none")]
+        //TODO: Make generic cache that recaches the top from all categories
+        [OutputCache(Duration = 5 * 60, VaryByParam = "none")]
         public ActionResult GetSlider()
         {
             var model = this.postsService.GetTopCountPostsByRatingInEveryCategory(4).To<CategoryAndPostsViewModel>().ToList();
-
-
             return this.PartialView("_Slider", model);
+        }
+
+        [OutputCache(Duration = 5 * 60, VaryByParam = "none")]
+        public ActionResult GetSideBar()
+        {
+            var model = this.postsService.GetTopCountPostsByRatingInEveryCategory(4).To<CategoryAndPostsViewModel>().ToList();
+            return this.PartialView("_Sidebar", model);
         }
     }
 }
