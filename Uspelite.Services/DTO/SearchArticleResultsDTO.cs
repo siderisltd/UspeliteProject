b51d1c6d74a16ffaf4c6a312295bc26b0fe36776
lexsideris@gmail.com <lexsideris@gmail.com>
2016-05-26 14:48:27 +0300

    Fixed ClientNavigation and included not included styles

---
+++ Uspelite.Services/DTO/SearchArticleResultsDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Uspelite.Services.Data.DTO
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using NinjaNye.SearchExtensions.Models;
+    using Uspelite.Data.Models;
+
+    public class SearchArticleResultsDTO
+    {
+        public IOrderedQueryable<IRanked<Article>> ResultsInTitles { get; set; }
+
+        public IOrderedQueryable<IRanked<Article>> ResultsInContents { get; set; }
+
+        public int AllSearchedResultsCount => this.ResultsInTitles.Count() + this.ResultsInContents.Count();
+    }
+}
